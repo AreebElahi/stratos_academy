9ccdf9d28f923b010156dffcfbf1e38436e41fb0
Areeb Elahi <106818935+AreebElahi@users.noreply.github.com>
2026-05-30 22:17:12 +0500

    docs: update README with course registration flow steps and rename new courses screenshot to Courses-Screen.png

--- lib/views/tasks_screen.dart
+++ lib/views/tasks_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'theme/app_theme.dart';
-import 'widgets/glass_panel.dart';
 
 class TasksScreen extends StatefulWidget {
   const TasksScreen({super.key});
